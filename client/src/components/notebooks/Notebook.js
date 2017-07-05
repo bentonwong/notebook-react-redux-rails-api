298bb486c04e56c95b4789c70b1b30cc25283656
bentonwong <bentonwong@gmail.com>
2017-07-05 16:27:39 -0700

    add notebook component

--- client/src/components/notebooks/Notebook.js
+++ client/src/components/notebooks/Notebook.js
@@ -1,2 +1,16 @@
 import React, { Component } from 'react';
 import NoteInput from '../notes/NoteInput'
+
+class Notebook extends Component {
+  render() {
+    return {
+      <div>
+        <li>{this.props.notebook.title}
+          <NoteInput store={this.props.store} notebookId={this.props.notebook.id}
+        </li>
+      </div>
+    }
+  }
+}
+
+export default Notebook;
