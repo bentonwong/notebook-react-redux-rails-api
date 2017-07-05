54736bea7500a5a588ec3058b2bdd17590df2190
bentonwong <bentonwong@gmail.com>
2017-07-05 14:28:53 -0700

    add notebooks resource

---
+++ app/controllers/notebooks_controller.rb
@@ -0,0 +1,35 @@
+class NotebooksController < ApplicationController
+  before_action :set_notebook, only: [:show, :update, :destroy]
+
+  def index
+    @notebooks = Notebook.all
+    render json: @notebooks.to_json, :layout => false
+  end
+
+  def create
+    @notebook = Notebook.create(notebook_params)
+    render json: @notebook.to_json, :layout => false
+  end
+
+  def show
+    render json: @notebook.to_json, :layout => false
+  end
+
+  def update
+    @notebook.update(notebook_params)
+  end
+
+  def destroy
+    @notebook.destroy
+  end
+
+  private
+
+    def notebook_params
+      params.require(:notebook).permit(:title)
+    end
+
+    def set_note
+      @notebook = Notebook.find_by(id: params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705210544) do
+ActiveRecord::Schema.define(version: 20170705212444) do
+
+  create_table "notebooks", force: :cascade do |t|
+    t.text     "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.text     "title"
