2f808ddd28c3c8c334f9468db2e30c7f3d96eb07
bentonwong <bentonwong@gmail.com>
2017-07-05 16:16:35 -0700

    add construtor and listeners to NotebookInput

--- client/src/components/notebooks/NotebookInput.js
+++ client/src/components/notebooks/NotebookInput.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 class NotebookInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+    }
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      title: event.target.value,
+    })
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+  }
+  
   render() {
     return (
       <div>
         <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <input type="text" value={this.state.text} onChange={(event) => this.handleOnChange(event)}/>
+          <input type="text" value={this.state.title} onChange={(event) => this.handleOnChange(event)}/>
           <input type="submit"/>
         </form>
       </div>
     )
   }
 }
+
+export default NotebookInput;
