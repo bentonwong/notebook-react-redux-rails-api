775dc013b3db1161b4c291a4e9003243d2f6d3ef
bentonwong <bentonwong@gmail.com>
2017-07-06 22:42:49 -0700

    format output of notebook-details container

--- client/src/containers/notebook-detail.js
+++ client/src/containers/notebook-detail.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class NotebookDetail extends Component {
+class NotebookDetail extends Component {
   render() {
+    if (!this.props.notebook) {
+      return <div>Select a notebook to on the left</div>
+    }
+    const notes = this.props.notebook.notes.map(note => {
+      return <div><h4>{note.title}</h4><p>{note.body}</p></div>
+    })
     return (
-      <div>Notebook detail</div>
+      <div>
+        <div><h3>{this.props.notebook.title}</h3></div>
+        <hr />
+        <div>{notes}</div>
+      </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    notebook: state.activeNotebook
+  };
+}
+
+export default connect(mapStateToProps)(NotebookDetail);
--- client/src/reducers/reducer_notebooks.js
+++ client/src/reducers/reducer_notebooks.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Chemistry' },
-    { title: 'Spanish' },
-    { title: 'English' },
-    { title: 'Art' }
+    { title: 'Chemistry', notes: [{title: "Hydrogen", body: "1 electron"}, {title: "Helium", body: "Noble gas"}] },
+    { title: 'Spanish', notes: [{title: "hola", body: "means hello"}, {title: "adios", body: "means bye"}] },
+    { title: 'English', notes: [{title: "Shakespeare", body: "to be or not to be"}, {title: "Mark Twain", body: "Huck Finn"}] },
+    { title: 'Art', notes: [{title: "abstract art", body: "very abstract"}, {title: "modern art", body: "very modern"}] }
   ]
 }
